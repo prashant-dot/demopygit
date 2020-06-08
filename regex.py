8affbdb02ec7984bb5488cccc1fe34c86eeb0a44
prashant-dot <prashantsuthar777@gmail.com>
2020-06-08 13:52:18 +0530

    code change

--- regex.py
+++ regex.py
@@ -1,1 +1,9 @@
-print("regex")
+import re
+def fun(pat):
+    if (re.search(pat, no)):
+        print('valid')
+    else:
+        print('invalid')
+pat='((0|[+]?91)?[-]?[7-9][0-9]{8}[0])'
+no=input('enter phone number')
+fun(no)
